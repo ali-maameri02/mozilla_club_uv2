099547f13150cbd15348922b7461ec681d3162cc
ali-maameri02 <127658525+ali-maameri02@users.noreply.github.com>
2023-07-29 22:33:27 +0100

    Add files via upload

---
+++ dev team/log.js
@@ -0,0 +1,19 @@
+
+var button=document.getElementById("join");
+var closing=document.getElementById("close");
+button.addEventListener("click",Showlog);
+closing.addEventListener("click",closeform);
+function Showlog() {
+    document.getElementById("sign").style.display="block";
+}
+function closeform(){
+    document.getElementById("sign").style.display="none";
+    
+}
+var form=document.getElementById("sign-form");
+var send =document.getElementById("ok");
+
+send.addEventListener("click",message);
+function message(){
+    form.innerHTML="<p style='color:orange;'>Thanks for The inscription check your email we will contact you"+"<br>"+'<img src="mozlogo.png" width="40px" class="mozlogo">';
+}
